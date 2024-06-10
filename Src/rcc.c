36fcb9fb4c143f33fdd907d90fb63692f4b96e84
Nick Grigoriadis <44061694+nikosgri@users.noreply.github.com>
2024-06-10 10:22:13 +0300

    style: Change the style of the rcc.c file

--- Src/rcc.c
+++ Src/rcc.c
@@ -3,35 +3,35 @@
  *
  *  Created on: Jun 8, 2024
  *      Author: Nikolaos Grigoriadis
- *		Email : n.grigoriadis09@gmail.com
- *		Title : Embedded software engineer
- * 	 	Degree: BSc and MSc in computer science, university of Ioannina
+ *      Email : n.grigoriadis09@gmail.com
+ *      Title : Embedded software engineer
+ *      Degree: BSc and MSc in computer science, university of Ioannina
  */
 
 #include "rcc.h"
 
 void init_system(void)
 {
-	/*Enable high speed internal clock HSI16*/
-	RCC->CR |= RCC_CR_HSION;
+     /*Enable high speed internal clock HSI16*/
+     RCC->CR |= RCC_CR_HSION;
 
-	/*Wait until internal high speed clock is ready*/
-	while (RCC->CR & RCC_CR_HSIRDY) {}
+     /*Wait until internal high speed clock is ready*/
+     while (RCC->CR & RCC_CR_HSIRDY) {}
 
-	/*Do not devide the HSI*/
-	RCC->CR &= ~RCC_CR_HSIDIVEN;
+     /*Do not devide the HSI*/
+     RCC->CR &= ~RCC_CR_HSIDIVEN;
 
-	/*Select microcontroller output clock*/
-	MODIFY_REG(RCC->CFGR, RCC_CFGR_MCOSEL, (0x02 << RCC_CFGR_MCOSEL_Pos));
+     /*Select microcontroller output clock*/
+     MODIFY_REG(RCC->CFGR, RCC_CFGR_MCOSEL, (0x02 << RCC_CFGR_MCOSEL_Pos));
 
-	/*Select HSI16 as system clock*/
-	MODIFY_REG(RCC->CFGR, RCC_CFGR_SW, (0x01 << RCC_CFGR_SW_Pos));
+     /*Select HSI16 as system clock*/
+     MODIFY_REG(RCC->CFGR, RCC_CFGR_SW, (0x01 << RCC_CFGR_SW_Pos));
 
-	/*Wait until HSI16 becomes system clock*/
-	while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSI) {}
+     /*Wait until HSI16 becomes system clock*/
+     while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSI) {}
 
-	/*Update system core clock*/
-	SystemCoreClockUpdate();
+     /*Update system core clock*/
+     SystemCoreClockUpdate();
 }
 
 
