28f9aa9d22372c12224f13a42eb5508a4820177b
Nick Grigoriadis <44061694+nikosgri@users.noreply.github.com>
2024-06-10 10:22:40 +0300

    style

--- Src/uart.c
+++ Src/uart.c
@@ -3,9 +3,9 @@
  *
  *  Created on: Jun 8, 2024
  *      Author: Nikolaos Grigoriadis
- *		Email : n.grigoriadis09@gmail.com
- *		Title : Embedded software engineer
- * 	 	Degree: BSc and MSc in computer science, university of Ioannina
+ *      Email : n.grigoriadis09@gmail.com
+ *      Title : Embedded software engineer
+ *      Degree: BSc and MSc in computer science, university of Ioannina
  */
 
 
